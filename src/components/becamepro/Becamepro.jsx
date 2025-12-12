28cc22f7443139b607ed787b2a51a53c3a8392ec
Nitesh Kumar <Nitesh0942@gmail.com>
2025-12-12 17:54:49 +0530

    enhanacement

--- src/components/becamepro/Becamepro.jsx
+++ src/components/becamepro/Becamepro.jsx
@@ -15,6 +15,7 @@ export const Becamepro = () => {
             </p>
             <button className={styles.button}><Link to="/become-professional">Become a Pro</Link></button>
         </div>
+        
         <div className={styles.image}>
             <img src="./Becamepro/pro.png" alt="" />
         </div>
